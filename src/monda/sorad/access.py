8e5bf4014ea600013deccc83c0762cf28d6a2bf2
Nils Müller <nils.mueller@awi.de>
2026-04-21 22:17:37 +0200

    BUG: Crashes in time parsing for sorad.access.get_wfs
    Fixes three bugs:
    - Crash when len(timewindow) != 2
    - Crash when timewindow is neither tuple nor None
    - Crash when timewindow is a string (used strftime which converts
      datetime -> str)
    
    To fix the time-string parsing we use dateutil which is already a pandas dependency

--- src/monda/sorad/access.py
+++ src/monda/sorad/access.py
@@ -16,6 +16,7 @@ import json
 import urllib.request
 import urllib.parse
 import pandas as pd
+from dateutil.parser import parse as str2datetime
 
 log = logging.getLogger('sorad-downloader')
 myFormat = '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
@@ -33,25 +34,33 @@ def get_wfs(count=1000, platform=None, timewindow=None, layer='rsg:sorad_public_
     : str layer:        Specify the geoserver layer. For a list of available layers see https://rsg.pml.ac.uk/geoserver/. (default rsg:sorad_public_view_fp_rrs)
 
     """
-    if isinstance(timewindow, tuple):
-        if (isinstance(timewindow[0], str)) and (isinstance(timewindow[1], str)):
-            try:
-                time_start = datetime.datetime.strftime(timewindow[0], '%Y-%m-%dT%H:%M:%SZ')
-                time_end   = datetime.datetime.strftime(timewindow[1], '%Y-%m-%dT%H:%M:%SZ')
-            except:
-                log.error("timewindow should be a tuple of type datetime.datetime or iso-formatted string i.e. 'YYYY-mm-ddTHH:MM:SSZ'")
-                return None
-        elif (isinstance(timewindow[0], datetime.datetime)) and (isinstance(timewindow[1], datetime.datetime)):
-            time_start = timewindow[0]
-            time_end = timewindow[1]
-        else:
-            log.error("timewindow should be a tuple of type datetime.datetime or iso-formatted string i.e. 'YYYY-mm-ddTHH:MM:SSZ'")
+    if isinstance(timewindow, (list,tuple)):
+        if len(timewindow) != 2:
+            log.error("timewindow must be a tuple/list of length two or None (Defaults to last 24h)")
+            return None
+        if not all(isinstance(t, (datetime.datetime, str)) for t in timewindow):
+            log.error("timewindow must have type datetime.datetime or str")
+            return None
+        try:
+            time_start, time_end = timewindow
+            if isinstance(time_start, str):
+                time_start = str2datetime(time_start)
+            if isinstance(time_end, str):
+                time_start = str2datetime(time_end)
+        except:
+            log.error("timewindow: Failed to convert string to datetime (doc: dateutil.parse.parser")
             return None
     elif timewindow is None:
         # default to the last 24H
         time_start = datetime.datetime.now() - datetime.timedelta(days=1)
         time_end = datetime.datetime.now()
+    else:
+        log.error("timewindow must be a tuple/list of length two or None (Defaults to last 24h)")
+        return None
 
+    if time_start > time_end:
+        log.error(f"timewindow is empty ({time_start} to {time_end})")
+        return None
     # build a CQL filter to get a time slice
     cql = ''
 
